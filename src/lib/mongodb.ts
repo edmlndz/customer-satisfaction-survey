6928740e8e42d3610498f3db8955156a6f5b9d82
edmlndz <eduardomelendezcruz@gmail.com>
2025-09-10 15:16:00 -0600

    fix para conexion base de datos

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -5,7 +5,14 @@ if (!process.env.MONGODB_URI) {
 }
 
 const uri = process.env.MONGODB_URI
-const options = {}
+const options = {
+  ssl: true,
+  sslValidate: true,
+  retryWrites: true,
+  maxPoolSize: 10,
+  serverSelectionTimeoutMS: 5000,
+  socketTimeoutMS: 45000,
+}
 
 let client
 let clientPromise: Promise<MongoClient>
